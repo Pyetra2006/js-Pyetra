bc111f693683b5f2a26a0de340becfa318e3e1e1
Pyetra2006 <00001108590263sp@al.educacao.sp.gov.br>
2024-08-08 17:29:22 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(800, 800);
+  background("violet");
+}
+
+function draw() {
+  
+  
+  stroke("blue")
+  fill("pink")
+  
+  
+  //console.log(mouseIsPressed)
+  
+  if(mouseIsPressed)
+    
+
+  circle(mouseX, mouseY, 55 ,38)
+}
